4744e63a00868d7f6d45a172cb8be7d53a102ee4
Chen <neosdiva@gmail.com>
2021-06-25 15:42:36 -0300

    Projeto tranagulo

---
+++ Triangulo.java
@@ -0,0 +1,22 @@
+/*
+ * Crie um arquivo chamado
+Triangulo.java
+. Implemente um programa que exiba na saida padrao
+um triangulo de asteriscos semelhante ao mostrado a seguir. Nao utilize sequencias de escape para
+desenvolver esse programa. 
+ */
+
+
+public class Triangulo {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		
+	}
+}
+
